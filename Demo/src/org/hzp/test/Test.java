7490e967afb803c098da0bac5ba1d38dc21fa435
hzp <2540619242@qq.com>
2023-02-27 16:23:18 +0800

    第一次修改

--- Demo/src/org/hzp/test/Test.java
+++ Demo/src/org/hzp/test/Test.java
@@ -2,6 +2,8 @@ package org.hzp.test;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("修改");
         System.out.println("Git测试");
+        System.out.println("添加一句代码");
     }
 }
